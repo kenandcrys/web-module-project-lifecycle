639477a2f19c83e43c8d2bac6427072743292c9a
Kenneth Fisher <kenandcrys@live.com>
2023-06-22 16:10:54 -0500

    Begin App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    todos: []
+    }
+  }
+
+componentDidMount(){
+  axios.get(URL)
+  .then(res => {
+      this.setState({ ...this.state, todos: res.data})
+  })
+  .catch(e =>{
+    console.log(`No you didn't!!!`)
+  })
+}
+
   render() {
     return null
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <form>
+        <input placeholder='Todo Here' />
+        <button>add Todo</button>
+      </form>
+      );
   }
 }
